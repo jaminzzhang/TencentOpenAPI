f8603c7d1ac78bd3eca48e602cdf4ac5ac647fab
Jamin Zhang <jaminzzhang@gmail.com>
2014-08-22 14:51:02 +0800

    [Init]: Add Tencent Open SDK

---
+++ sample/TencentOAuthDemo/Classes/ConfigMacros.h
@@ -0,0 +1,41 @@
+
+#define JS_RETURN_TYPR NSString*
+#define JS_TYPE_BOOL NSString*
+#define JS_TYPE_YES @"true"
+#define JS_TYPE_NO @"false"
+#ifndef __LOGIN_ENABLE__
+#define __LOGIN_ENABLE__ 1
+#endif
+
+#ifndef __SDK_ENABLE__
+#define __SDK_ENABLE__ 1
+#endif
+
+#ifndef __SET_USER_HEADPIC_ENABLE__
+#define __SET_USER_HEADPIC_ENABLE__ 1
+#endif
+
+#ifndef __QQAPI_ENABLE__
+#define __QQAPI_ENABLE__ 1
+#endif
+
+#ifndef __TENCENT_API_ENABLE__
+#define __TENCENT_API_ENABLE__ 1
+#endif
+
+#ifndef __SEND_STORY_ENABLE__
+#define __SEND_STORY_ENABLE__ 1
+#endif
+
+#ifndef __MTA_ENABLE__
+#define __MTA_ENABLE__ 1
+#endif
+
+#ifndef __WEIYUNAPI_ENABLE__
+#define __WEIYUNAPI_ENABLE__ 1
+#endif
+
+#ifndef __WEIBOAPI_ENABLE__
+#define __WEIBOAPI_ENABLE__ 1
+#endif
+
